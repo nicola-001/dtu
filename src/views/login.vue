71effe1f4264bd418648a9df80646fbc6a5d88ad
nicola <2629137145@qq.com>
2025-07-29 22:33:04 +0800

    登陆

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,62 +1,64 @@
 <template>
   <div class="login">
-    <el-form ref="loginRef" :model="loginForm" :rules="loginRules" class="login-form">
-      <h3 class="title">{{ title }}</h3>
-      <el-form-item prop="username">
-        <el-input
-          v-model="loginForm.username"
-          type="text"
-          size="large"
-          auto-complete="off"
-          placeholder="账号"
-        >
-          <template #prefix><svg-icon icon-class="user" class="el-input__icon input-icon" /></template>
-        </el-input>
-      </el-form-item>
-      <el-form-item prop="password">
-        <el-input
-          v-model="loginForm.password"
-          type="password"
-          size="large"
-          auto-complete="off"
-          placeholder="密码"
-          @keyup.enter="handleLogin"
-        >
-          <template #prefix><svg-icon icon-class="password" class="el-input__icon input-icon" /></template>
-        </el-input>
-      </el-form-item>
-      <el-form-item prop="code" v-if="captchaEnabled">
-        <el-input
-          v-model="loginForm.code"
-          size="large"
-          auto-complete="off"
-          placeholder="验证码"
-          style="width: 63%"
-          @keyup.enter="handleLogin"
-        >
-          <template #prefix><svg-icon icon-class="validCode" class="el-input__icon input-icon" /></template>
-        </el-input>
-        <div class="login-code">
-          <img :src="codeUrl" @click="getCode" class="login-code-img"/>
-        </div>
-      </el-form-item>
-      <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
-      <el-form-item style="width:100%;">
-        <el-button
-          :loading="loading"
-          size="large"
-          type="primary"
-          style="width:100%;"
-          @click.prevent="handleLogin"
-        >
-          <span v-if="!loading">登 录</span>
-          <span v-else>登 录 中...</span>
-        </el-button>
-        <div style="float: right;" v-if="register">
-          <router-link class="link-type" :to="'/register'">立即注册</router-link>
-        </div>
-      </el-form-item>
-    </el-form>
+   <el-card>
+     <el-form ref="loginRef" :model="loginForm" :rules="loginRules" class="login-form">
+       <h3 class="title">{{ title }}</h3>
+       <el-form-item prop="username">
+         <el-input
+             v-model="loginForm.username"
+             type="text"
+             size="large"
+             auto-complete="off"
+             placeholder="账号"
+         >
+           <template #prefix><svg-icon icon-class="user" class="el-input__icon input-icon" /></template>
+         </el-input>
+       </el-form-item>
+       <el-form-item prop="password">
+         <el-input
+             v-model="loginForm.password"
+             type="password"
+             size="large"
+             auto-complete="off"
+             placeholder="密码"
+             @keyup.enter="handleLogin"
+         >
+           <template #prefix><svg-icon icon-class="password" class="el-input__icon input-icon" /></template>
+         </el-input>
+       </el-form-item>
+       <el-form-item prop="code" v-if="captchaEnabled">
+         <el-input
+             v-model="loginForm.code"
+             size="large"
+             auto-complete="off"
+             placeholder="验证码"
+             style="width: 63%"
+             @keyup.enter="handleLogin"
+         >
+           <template #prefix><svg-icon icon-class="validCode" class="el-input__icon input-icon" /></template>
+         </el-input>
+         <div class="login-code">
+           <img :src="codeUrl" @click="getCode" class="login-code-img"/>
+         </div>
+       </el-form-item>
+       <el-checkbox v-model="loginForm.rememberMe" style="margin:0px 0px 25px 0px;">记住密码</el-checkbox>
+       <el-form-item style="width:100%;">
+         <el-button
+             :loading="loading"
+             size="large"
+             type="primary"
+             style="width:100%;"
+             @click.prevent="handleLogin"
+         >
+           <span v-if="!loading">登 录</span>
+           <span v-else>登 录 中...</span>
+         </el-button>
+         <div style="float: right;" v-if="register">
+           <router-link class="link-type" :to="'/register'">立即注册</router-link>
+         </div>
+       </el-form-item>
+     </el-form>
+   </el-card>
     <!--  底部  -->
     <div class="el-login-footer">
       <span>Copyright © 2018-2025 ruoyi.vip All Rights Reserved.</span>
@@ -174,8 +176,9 @@ getCookie()
   justify-content: center;
   align-items: center;
   height: 100%;
-  background-image: url("../assets/images/login-background.jpg");
+  //background-image: url("../assets/images/login-background.jpg");
   background-size: cover;
+
 }
 .title {
   margin: 0px auto 30px auto;
